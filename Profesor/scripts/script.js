1f1500c83566767165d4c14a8e831e493b0f0929
angeldavila00 <angeldavila0975@gmail.com>
2025-07-18 12:24:13 -0500

    feat :sparkles: java startup

---
+++ Profesor/scripts/script.js
@@ -0,0 +1,56 @@
+// #####################
+//##### DIA 1 ##########
+// #####################
+
+// ¡¡¡¡PRIMER COMANDO !!!!
+console.log("Hola Mundo");
+//Number
+variable1=3.14; /* siempre poner punto y coma*/
+console.log(variable1)
+console.log(typeof (variable1)) //typeof es el tipo de caracter que la variable1 esta guardando (en este caso es NUMBER)
+//concatenacion de palabras
+palabra1="Campus"
+palabra2="Lands"
+palabra3=palabra1+palabra2;
+console.log(palabra3);
+
+//ejemplo
+
+nombrePersona="Luis";
+plata=55000;
+console.log("la persona "+ nombrePersona +" tiene un saldo de " +plata + " pesos");
+
+//Booleanos
+
+booleanito1=false;
+booleanito2=false;
+console.log(typeof booleanito1);
+
+//Condicionales
+
+if(booleanito1 == true){
+    console.log("es verdadero");
+}
+else{
+    console.log("No es verdadero");
+}
+//Funciones bucles
+for(i=0;1<4;++) {
+    console.log("Buenas");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Desarrollado por: Angel Niño - C.C 1.005.335.914
