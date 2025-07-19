ea6448b6d768fb95141bdbf4cc39dc8b41236279
Angel <angeldavila0975@gmail.com>
2025-07-19 18:38:56 -0500

    feat: :sparkles: getting started with javascript work

---
+++ DIA1/scripts/script.js
@@ -0,0 +1,8 @@
+console.log("Simulador de gastos diarios ")
+console.log("1. Registrar Nuevo gasto")
+console.log("2. Lista de Gastos")
+console.log("3. calcular total de gastos")
+console.log("4. Generar reporte de gastos")
+console.log("5. Lista de Gastos")
+console.log("6. Salir")
+
