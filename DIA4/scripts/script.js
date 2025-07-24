7464ba94d99a7eaa696f7b9ead3fd98f25b9738a
Angel <angeldavila0975@gmail.com>
2025-07-23 19:19:53 -0500

    feat :sparkles: chefs module ready

--- DIA4/scripts/script.js
+++ DIA4/scripts/script.js
@@ -179,3 +179,76 @@ function gestionarCategorias() {
         }
     }
 }
+
+// Gestionar chefs
+function gestionarChefs() {
+    let booleano = true;
+    while (booleano === true) {
+
+        let opcion = prompt(
+            'Bienvenido a la hamburgueseria sesion de categoria\
+        \n1. agregar chefs\
+        \n2. Ver chefs\
+        \n3. Eiminar chefs\
+        \n4. editar chefs\
+        \n5. Volver al menu principal')
+        if (opcion === "1") {
+            let nombre = prompt('Ingresa el nombre del chef que quieres agregar: ');
+            let especialidad = prompt('Cual es la especialidad: ')
+
+            chefs.push({
+                "nombre": nombre,
+                "especialidad": especialidad,
+
+
+            });
+        }
+        else if (opcion === "2") {
+            let leng = chefs.length;
+            for (let i = 0; i < leng; i++) {
+                alert(
+                    "Numero chef# " + (i + 1) + "\n" +
+                    "Nombre: " + chefs[i]["nombre"] + "\n" +
+                    "Especialidad: " + chefs[i]["especialidad"]
+
+                )
+
+            }
+        }
+        else if (opcion === "3") {
+            let leng = chefs.length;
+            for (let i = 0; i < leng; i++) {
+                alert(
+                    "Numero Chef# " + (i + 1) + "\n" +
+                    "Nombre: " + chefs[i]["nombre"] + "\n" +
+                    "Especialidad: " + chefs[i]["especialidad"]
+
+                )
+
+            }
+            let eli = prompt("Dime el numero del chef que deseas eliminar: ");
+            chefs.splice(eli - 1, 1);
+        }
+        else if (opcion === "4") {
+            let leng = chefs.length;
+            for (let i = 0; i < leng; i++) {
+                alert(
+                    "Numero Chef# " + (i + 1) + "\n" +
+                    "Nombre: " + chefs[i]["nombre"] + "\n" +
+                    "Especialidad: " + chefs[i]["especialidad"]
+
+                )
+
+            }
+            let num = prompt('Ingresa el numero del usuario que quieres editar: ');
+            let nam = prompt('Dime el nuevo nombre del ingrediente:');
+            let desc = prompt('Dime la especialidad: ');
+            chefs[num - 1]["nombre"] = nam;
+            chefs[num - 1]["especialidad"] = desc;
+        }
+        else if (opcion === "5") {
+            booleano = false;
+            alert("Regresando al menu principal")
+        }
+    }
+}
