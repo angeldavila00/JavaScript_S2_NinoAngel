c4af2ff6a1aa32fff8c0d38f55f99520c994eb9f
angeldavila00 <angeldavila0975@gmail.com>
2025-08-08 12:41:52 -0500

    feat: :sparkles: nwe folder day 15

--- STARWARSAPI/scripts/scripts.js
+++ STARWARSAPI/scripts/scripts.js
@@ -1,8 +1,8 @@
-function buscarPesonaje() {
+function BuscarPersonaje() {
     //document.getElementById("resultados").innerHTML=``;
     const nombreUsar = document.getElementById("nombreInput").value.trim();
     const xhr = new XMLHttpRequest(); //La importancia del XML
-    const url = `https://swapi.py4e.com/api/people/${nombreUsar}`;
+    const url = `https://swapi.py4e.com/api/people/?search=${nombreUsar}`;
     console.log(url);
     xhr.open("GET", url, true);
 
@@ -11,35 +11,31 @@ function buscarPesonaje() {
 
             console.log("Cargando...");
         }
-        else if (xhr.readyState === 4 && xhr.status == 200){
+        else if (xhr.readyState === 4 && xhr.status == 200) {
             try {
                 let data = JSON.parse(xhr.responseText);
+                console.log(data)
                 if (data.results && data.results.length > 0) {
-                    for (let i = 0; i < data.results.length;i++){
-                        let division = document.getElementById("resultados");
-                        division.innerHTML+= `
-                        
-                        
-                        
-                        
+                    let division = document.getElementById("resultados");
+                    division.innerHTML += `
+                        <div class="card">
+                        <h1>${data["results"][0]["name"]}</h1>
+                        <p>${data["results"][0]["eye_color"]}</p>
+                        <p>${data["results"][0]["birth_year"]}</p>
+                        <p>${data["results"][0]["mass"]}</p>
+                        </div>
                         `
-
-                    }
-
                 }
+                console.log(division)
             }
             catch (err) {
 
                 console.log(err.message);
-        }
+            }
         }
 
-        
-        
-        }
-        xhr.send();
+
+
     }
-    
-
-
-buscarPesonaje();
+    xhr.send();
+}
